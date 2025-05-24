943b570726b31d1148782bac2a6688ea4f028ba8
Marius Preikschat <m.preikschat@googlemail.com>
2025-05-24 15:25:07 +0200

    Initial Commit

---
+++ Screenshot Prevention/Screenshot_PreventionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_PreventionApp.swift
+//  Screenshot Prevention
+//
+//  Created by Marius Preikschat on 24.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Screenshot_PreventionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
